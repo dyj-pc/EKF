831ccfb0c55b96e6158561fef18a289e86a2af84
dyj-pc <1632403204@qq.com>
2025-08-23 21:27:08 +0800

    shanchu

--- 26/build/auto_aim/rosidl_generator_py/auto_aim/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from auto_aim.msg._gimbal_command import GimbalCommand  # noqa: F401
-from auto_aim.msg._serial_data import SerialData  # noqa: F401
--- 26/install/auto_aim/share/auto_aim/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(auto_aim_IDL_FILES "msg/SerialData.idl;msg/GimbalCommand.idl")
-set(auto_aim_INTERFACE_FILES "msg/SerialData.msg;msg/GimbalCommand.msg")
